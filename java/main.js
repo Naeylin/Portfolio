319b2a22a797cf2295e2b5686dafef063d82c9d5
Naeylin <yannvallet2097@gmail.com>
2021-11-22 16:34:46 +0100

    interactions navbar

---
+++ java/main.js
@@ -0,0 +1,27 @@
+let currentElem = null;
+
+flex_cont1.onmouseover = function(event) {
+    if (currentElem) return;
+    let target = event.target.closest('nav');
+    if (!target) return;
+    if (!flex_cont1.contains(target)) return;
+    currentElem = target;
+    onEnter(currentElem)
+};
+
+flex_cont1.onmouseout = function(event) {
+    if (!currentElem) return;
+    let relatedTarget = event.relatedTarget;
+    while (relatedTarget) {
+        if (relatedTarget == currentElem) return;
+        relatedTarget = relatedTarget.parentNode;
+    }
+    onLeave(currentElem);
+    currentElem = null;
+}
+function onEnter(elem) {
+    elem.style.background = 'gold';
+}
+function onLeave(elem) {
+    elem.style.background = 'blue';
+}
